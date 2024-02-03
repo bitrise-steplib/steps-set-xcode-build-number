65ab9d06a5cf4d344c3b58addb9ad8e7d5d588d9
Szabolcs Toth <szabolcs.toth@bitrise.io>
2024-02-03 22:23:58 +0000

    Add test projects

---
+++ testdata/project/Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Szabolcs Toth on 02/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
